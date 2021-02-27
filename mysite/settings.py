ac83ef26dd9c96ef6c05497093fb498e98171972
Roberto Adolfo Herrera Soriano <21351766+robsorRS@users.noreply.github.com>
2021-02-27 18:46:51 -0400

    Update mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '63hlw(@u-86usk5@!+_+(l$$i30w5pp@y&j)7l$fimud_air1a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['robsorrs.pythonanywhere.com']
+ALLOWED_HOSTS = ['robsorrs.pythonanywhere.com', '192.168.10.10']
 
 
 # Application definition
